fb3af3cf322ab688467dd113cb986481d373ec8d
Robert Dziarnowski <rdziarnowski@egnyte.com>
2021-12-11 00:48:13 +0100

    update client config.

--- frp/run.sh
+++ frp/run.sh
@@ -7,7 +7,7 @@ SERVER_IP=$(jq --raw-output '.server_ip' $CONFIG_PATH)
 AUTH_TOKEN=$(jq --raw-output '.auth_token // empty' $CONFIG_PATH)
 SERVER_PORT=$(jq --raw-output '.server_port' $CONFIG_PATH)
 LOCAL_PORT=$(jq --raw-output '.local_port' $CONFIG_PATH)
-REMOTE_PORT=$(jq --raw-output '.remote_port' $CONFIG_PATH)
+CUSTOM_DOMAINS=$(jq --raw-output '.custom_domains' $CONFIG_PATH)
 PROXY_NAME=$(jq --raw-output '.proxy_name // empty' $CONFIG_PATH)
 
 FRP_PATH=/var/frp
@@ -18,7 +18,7 @@ if [ -f $FRPC_CONF ]; then
 fi
 
 if [ ! $PROXY_NAME ]; then
-  PROXY_NAME=hassio
+  PROXY_NAME=web
   echo Using default proxy name $PROXY_NAME
 fi
 
@@ -29,10 +29,11 @@ if [ $AUTH_TOKEN ]; then
   echo "privilege_token = $AUTH_TOKEN" >> $FRPC_CONF
 fi
 echo "[$PROXY_NAME]" >> $FRPC_CONF
-echo "type = tcp" >> $FRPC_CONF
+echo "type = http" >> $FRPC_CONF
 echo "local_ip = 127.0.0.1" >> $FRPC_CONF
 echo "local_port = $LOCAL_PORT" >> $FRPC_CONF
-echo "remote_port = $REMOTE_PORT" >> $FRPC_CONF
+echo "custom_domains = $CUSTOM_DOMAINS" >> $FRPC_CONF
+
 
 echo Start frp as client
 
